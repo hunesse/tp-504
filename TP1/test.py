b4781faa1c33d408a549ce67c49059665db11a7d
kermagae <gaetan.kermarrec.gk@gmail.com>
2024-09-16 16:24:46 +0200

    ajout test limite

--- TP1/test.py
+++ TP1/test.py
@@ -8,3 +8,10 @@ def test_1():
 def test_2():
 	assert f.puissance(2,-2)==0.25
 	assert f.puissance(-8,2)==64
+
+def test_3():
+	assert f.puissance(-2,2)==4
+	assert f.puissance(-2,3)==-8
+	assert f.puissance(2,0)==1
+	assert f.puissance(-2,0)==-1
+	assert f.puissance(2,3)==8
