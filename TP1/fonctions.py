869cff21db47455b90918f04c0910c7f9aa0b17f
kermagae <gaetan.kermarrec.gk@gmail.com>
2024-09-16 16:47:29 +0200

    version avec boucle

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -4,13 +4,12 @@ def puissance(a,b):
 #			raise TypeError("Uniquement des chiffre entier")
 	#return a**b
 	
-  if b == 0:
-    return 1
-  elif b > 0:
-    resultat = a
-    for _ in range(1, b):
-      resultat *= a
-    return resultat
-  else:
-    # Pour les exposants négatifs, on calcule l'inverse de a^|b|
-    return 1 / puissance(a, -b)
+	if b == 0:
+		return 1
+	elif b > 0:
+		resultat = a
+		for _ in range(1, b):
+			resultat *= a
+		return resultat
+	else:
+		return 1 / puissance(a, -b)
