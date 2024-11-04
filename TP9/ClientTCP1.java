39e60bb19d68797986eff428753247a53239097d
gaetan kermarrec <gaetan.kermarrec.gk@gmail.com>
2024-11-04 16:47:57 +0100

    ajout TP1

--- TP9/ClientTCP1.java
+++ TP9/ClientTCP1.java
@@ -4,10 +4,10 @@ import java.net.*;
 public class ClientTCP1 {
     public static void main(String[] args){
         try{
-
-
-
-        
+            Socket socket = new Socket("localhost",2016);
+            DataOutputStream dOut = new DataOutputStream(socket.getOutputStream());
+            dOut.writeUTF("message test"); 
+            socket.close();
         }catch(Exception ex){System.out.println("ALARME!!!");}
     }
 }
--- TP9/ServeurTCP1.java
+++ TP9/ServeurTCP1.java
@@ -4,10 +4,14 @@ import java.net.*;
 public class ServeurTCP1 {
     public static void main(String[] args){
         try{
-            
-
-
-        
+            ServerSocket socketserver = new ServerSocket(2016);
+            System.out.println("server en attente");
+            Socket socket = socketserver.accept();
+            System.out.println("Connection d'un client");
+            DataInputStream dIn = new DataInputStream(socket.getInputStream());
+            System.out.println("Message : "+ dIn.readUTF());
+            socket.close();
+            socketserver.close();
         }catch(Exception ex){System.out.println("ALARME!!!");}
     }
 }
