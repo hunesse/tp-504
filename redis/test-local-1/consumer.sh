79fe89105f7ec6b45e152ed6a979ec30ef66ff2d
user <gaetan.kermarrec.gk@gmail.com>
2025-04-30 15:32:37 +0200

    final

--- redis/test-local-1/consumer.sh
+++ redis/test-local-1/consumer.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
 
-QUEUE_NAME="mafile"
-BURST_SIZE=1000
-BURST_DELAY=3  
+QUEUE_NAME="QUEUE"
+THRESHOLD=30000
+DELAY_PROCESS=4 
+CSV_FILE="issue.csv"
 REDIS_CLI="redis-cli"
 
 $REDIS_CLI DBSIZE >/dev/null
 if ! [ $? = 0 ]; then
   echo "Erreur, pas de connection avec le serveur redis!"
   exit 1
+fi	
+
+if [ ! -f "$CSV_FILE" ]; then
+  echo "timestamp;threshold;value" > "$CSV_FILE"
 fi
 
 while :
-do
-  echo "Envoi d’un burst de $BURST_SIZE valeurs..."
-  for ((i=0; i<BURST_SIZE; i++)); do
-    $REDIS_CLI LPUSH "$QUEUE_NAME" $RANDOM >/dev/null
-  done
+do	
+  ls=$($REDIS_CLI --raw LLEN "$QUEUE")
+  
+  if [ "$ls" -gt 0 ]; then
+    value=$($REDIS_CLI --raw RPOP "$QUEUE")
+    timestamp=$(date +"%Y-%m-%d %H:%M:%S")
+    
+    echo "$timestamp;$THRESHOLD;$value" >> "$CSV_FILE"
+    echo "Valeur reçue : $value"
 
-  TAILLE=$($REDIS_CLI LLEN "$QUEUE_NAME")
-  echo "Taille actuelle de la file : $TAILLE"
-
-  # Pause entre les bursts
-  sleep "$BURST_DELAY"
+    if [ "$value" -gt "$THRESHOLD" ]; then
+      echo "ALARME ! Valeur élevée : $value"
+      sleep "$DELAY_PROCESS"
+    fi
+  else
+    echo "Liste vide, attente 2s."
+    sleep 2
+  fi
 done
 
--- redis/test-local-1/producer.sh
+++ redis/test-local-1/producer.sh
@@ -1,13 +1,21 @@
-redis-cli DBSIZE >/dev/null
-if ! [ $? = 0 ]
-then
-	echo "Erreur, pas de connection avec le serveur redis!"
-exit 1
+#!/bin/bash
+
+QUEUE_NAME="QUEUE"
+BURST_SIZE=1000
+BURST_DELAY=3  # secondes
+REDIS_CLI="redis-cli"
+
+$REDIS_CLI DBSIZE >/dev/null
+if ! [ $? = 0 ]; then
+  echo "Erreur, pas de connection avec le serveur redis!"
+  exit 1
 fi
+
 while :
-	for ((i=0;i<n;i++))
-	do
-		redis-cli LPUSH mafile $RANDOM
-	done
+do
+  for ((i=0; i<BURST_SIZE; i++)); do
+    $REDIS_CLI LPUSH "$QUEUE" $RANDOM >/dev/null
+  done
+  sleep "$BURST_DELAY"
 done
 
